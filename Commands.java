92734e73a0d89c0a9cb1bb0ac85e2e4c8fca24f8
Fatemeh Rajaee <fatemehrajaee15@gmail.com>
2023-04-25 17:08:55 +0430

    view : Commands Class added

---
+++ Commands.java
@@ -0,0 +1,41 @@
+package view;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public enum Commands {
+    REGISTER_VALID("user create .+"),
+    LOGIN_VALID("user login .+"),
+    FORGOT_PASSWORD("forgot my password .+"),
+    SECURITY_QUESTION("question pick .+"),
+    RANDOM_PASSWORD_CREATE("user create .+"),
+    RANDOM_SLOGAN_CREATE("user create .+"),
+    //FORGOT_PASS_WORD("forgot my password"),
+    USER_LOGOUT("user logout"),
+
+    USERNAME_CHANGE("profile change -u (?<username>(\".+\")|(\\S+))"),
+    NICKNAME_CHANGE("profile change -n (?<nickname>(\".+\")|(\\S+))"),
+    PASSWORD_CHANGE("profile change password -o (?<old-password>(\".+\")|(\\S+)) -n (?<new-password>(\".+\")|(\\S+))"),
+    EMAIL_CHANGE("profile change -e (?<email>.+)"),
+    SLOGAN_CHANGE("profile change slogan -s (?<slogan>.*)"),
+    REMOVE_SLOGAN("Profile remove slogan"),
+    HIGHSCORE_DISPLAY("profile display highscore"),
+    RANK_DISPLAY("profile display rank"),
+    SLOGAN_DISPLAY("profile display slogan"),
+    PROFILE_DISPLAY("profile display"),
+
+
+    private String regex;
+
+    private Commands(String regex) {
+        this.regex = regex;
+    }
+
+    public static Matcher getMatcher(String input, Commands command) {
+        Matcher matcher = Pattern.compile(command.regex).matcher(input);
+        if (matcher.matches()) {
+            return matcher;
+        }
+        return null;
+    }
+}

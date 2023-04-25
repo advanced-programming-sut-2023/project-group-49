db07aae96faf95192d572844bdd0d9ffd17961fb
Mobian Heidari <mobinaheidari04@gamil.com>
2023-04-25 18:59:54 +0430

    controller: add game menu controller added

---
+++ GameMenuController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.util.ArrayList;
+
+public class GameMenuController {
+    public Game game;
+    private Game currentGame;
+    private ArrayList<Request> requests=new ArrayList<Request>();
+    private ArrayList<Request> requestAcceptedAndSent=new ArrayList<Request>();
+    public static Featurs showMap(int x,int y){
+        return null;
+    }
+    public static int showFoodRate(){
+        return null;
+    }
+}

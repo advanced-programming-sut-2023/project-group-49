b15a3c1de48ab89fb432d38a1175bd80e912f927
Fatemeh Rajaee <fatemehrajaee15@gmail.com>
2023-05-09 01:05:58 +0430

    Soldiers enum changed

--- src/main/java/model/Soldiers.java
+++ src/main/java/model/Soldiers.java
@@ -65,13 +65,7 @@ public enum Soldiers {
 
     }
 
-    public static Matcher getMatcher(String input, Commands command) {
-        Matcher matcher = Pattern.compile(command.regex).matcher(input);
-        if (matcher.matches()) {
-            return matcher;
-        }
-        return null;
-    }
+    
 
 
 }

96c83433ac794ef0e621bf1b94c8174dd9ceebd3
Mobina Heidari <mobinaheidari04@gamil.com>
2023-05-10 10:51:39 +0430

    Materials class changed

--- src/main/java/model/Materials.java
+++ src/main/java/model/Materials.java
@@ -1,4 +1,33 @@
 package model;
 
 public enum Materials {
+    GOLD("gold", 0),
+    WOOD("wood",0),
+    STONE("stone",0);
+
+    private String name;
+    private Player player;
+    private int amount;
+
+
+    private Materials(String name,int amount){
+        this.name=name;
+        this.amount=amount;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
 }

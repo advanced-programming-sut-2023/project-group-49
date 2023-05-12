5389f5c1938a2c9041a18dc7ce4f1c3cfd1e5566
atefe <atfghndhri19@gmail.com>
2023-05-12 12:35:56 +0430

    method showMap in GameMenuController updated

--- src/main/java/controller/GameMenuController.java
+++ src/main/java/controller/GameMenuController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import model.Map;
+import view.MapMenu;
+
 import java.util.ArrayList;
 
 public class GameMenuController {
@@ -7,8 +10,29 @@ public class GameMenuController {
     private Game currentGame;
     private ArrayList<Request> requests=new ArrayList<Request>();
     private ArrayList<Request> requestAcceptedAndSent=new ArrayList<Request>();
-    public static Featurs showMap(int x,int y){
-        return null;
+
+    public void showMap(int x, int y){
+        MapMenu.run();
+        for(){
+            Map.showApartOfMap(x,y);
+            if(Map.hasNotMilitary(x, y)==false){
+                System.out.println("S");
+                break;
+            } else if (Map.hasNotBuilding(x, y)==false){
+                if(Map.showApartOfMap(x,y).getListOfBuildings().contains(BuildingTypeCastle){
+                    System.out.println("w");
+                    break;
+                }
+                System.out.println("B");
+                break;
+            } else if (Map.hasNotTree(x,y)==false) {
+                System.out.println("T");
+                break;
+            } else {
+                System.out.println(Map.showApartOfMap(x,y).getGroundType());
+            }
+        }
+
     }
     public static int showFoodRate(){
         return null;

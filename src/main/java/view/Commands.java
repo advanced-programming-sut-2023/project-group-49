0e9f5bbaaf4cf1a086347378d1bb42b5fee78023
Fatemeh Rajaee <fatemehrajaee15@gmail.com>
2023-05-15 00:58:20 +0330

    Commands Enum changed!

--- src/main/java/view/Commands.java
+++ src/main/java/view/Commands.java
@@ -5,21 +5,31 @@ import java.util.regex.Pattern;
 
 public enum Commands {
     REGISTER_VALID("user create .+"),
-    LOGIN_VALID("user login -u (?<username>.*) -p (?<password>\\S*)(?<statLoggedIn>.*)"),
+    LOGIN_VALID("user login -u (?<username>.+) -p (?<password>.+) (?<statLoggedIn>.*)"),
     FORGOT_PASSWORD("forgot my password .+"),
     SECURITY_QUESTION("question pick .+"),
     USER_LOGOUT("user logout"),
-    USERNAME_CHANGE("profile change -u.+"),
-    NICKNAME_CHANGE("profile change -n.+"),
+    USERNAME_CHANGE("profile change .+"),
+    NICKNAME_CHANGE("profile change .+"),
     PASSWORD_CHANGE("profile change password .+"),
-    EMAIL_CHANGE("profile change -e.+"),
+    EMAIL_CHANGE("profile change .+"),
     SLOGAN_CHANGE("profile change slogan .+"),
-    REMOVE_SLOGAN("profile remove slogan"),
+    REMOVE_SLOGAN("Profile remove slogan"),
     HIGHSCORE_DISPLAY("profile display highscore"),
     RANK_DISPLAY("profile display rank"),
     SLOGAN_DISPLAY("profile display slogan"),
     PROFILE_DISPLAY("profile display"),
-    ENTER_MENU("enter menu (?<manuName>.+)");
+    ENTER_MENU("enter menu (?<manuName>.+)"),
+    SELECTUNIT("select unit.+"),
+    MOVEUNITTO("move unit to.+"),
+    PATROL_UNIT("patrol unit.+"),
+    SET("set.+"),
+    ATTACK_ENEMY("attack.+"),
+    AIR_ATTACK("attack.+"),
+    POUR_OIL("pour oil.+"),
+    DIG_TUNNEL("dig tunnel.+"),
+    BUILD("build.+"),
+    DISBANDUNIT("disband unit");
 
 
     private String regex;

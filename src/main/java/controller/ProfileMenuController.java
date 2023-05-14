83068c89ea8b254d6653cc24ff9a85b76f8b1b2e
Mobina Heidari <mobinaheidari04@gamil.com>
2023-05-14 21:10:23 +0430

    profile menu contoller edited

--- src/main/java/controller/ProfileMenuController.java
+++ src/main/java/controller/ProfileMenuController.java
@@ -3,38 +3,41 @@ package controller;
 import view.CommandsEnum;
 
 public class ProfileMenuController {
-    public static CommandsEnum changeUsername(String newUsername){
-       CommandsEnum result= SignupMenuController.checkUsername(newUsername);
-       if(result!=CommandsEnum.SUCCESS)
-           return result;
-       LoginMenuController.currentUser.setUsername(newUsername);
-       return CommandsEnum.SUCCESS;
+    public static CommandsEnum changeUsername(){
+        CommandsEnum result= SignupMenuController.checkUsername(SignupMenuController.getUsername());
+        if(result!=CommandsEnum.SUCCESS)
+            return result;
+        LoginMenuController.currentUser.setUsername(SignupMenuController.getUsername());
+        return CommandsEnum.SUCCESS;
     }
-    public static CommandsEnum changePassword(String oldPassword,String newPassword){
+    public static CommandsEnum changePassword(){
+        String oldPassword=SignupMenuController.getOldPassword();
+        String newPassword=SignupMenuController.getNewPassword();
         if(!LoginMenuController.currentUser.isPasswordCorrect(oldPassword))
             return CommandsEnum.WRONGPASSWORD;//check
-        if(oldPassword.equals(newPassword))return null;//TODO:create Enum
+        if(oldPassword.equals(newPassword))
+            return CommandsEnum.ENTER_NEW_PASSWORD;//TODO:create Enum
         CommandsEnum result=SignupMenuController.checkPasswordFormat(newPassword);
-        if(result!=CommandsEnum.SUCCESS)
-            return CommandsEnum.SUCCESS;
-        LoginMenuController.currentUser.setPassword(newPassword);
-        return CommandsEnum.SUCCESS;
+        return result;
 
     }
-    public static void changeNickname(String newNickname){
-        //TODO:errors
+    public static void changeNickname(){
+        String newNickname=SignupMenuController.getNickname();
         LoginMenuController.currentUser.setNickname(newNickname);
     }
-    public static CommandsEnum changeEmail(String newEmail){
+    public static CommandsEnum changeEmail(){
+        String newEmail=SignupMenuController.getEmail();
         CommandsEnum result=SignupMenuController.checkEmailFormats(newEmail);
         if(result!=CommandsEnum.SUCCESS)
             return result;
         LoginMenuController.currentUser.setEmail(newEmail);
         return CommandsEnum.SUCCESS;
     }
-    public static CommandsEnum changeSlogan(String newSlogan){
+    public static CommandsEnum changeSlogan(){
+        String newSlogan=SignupMenuController.getSlogan();
+        if(newSlogan.matches("\\s*"))
+            return CommandsEnum.EMPTY_FIELD;
         LoginMenuController.currentUser.setSlogan(newSlogan);
-        //TODO:errors
         return CommandsEnum.SUCCESS;
     }
     public static CommandsEnum removeSlogan(){
